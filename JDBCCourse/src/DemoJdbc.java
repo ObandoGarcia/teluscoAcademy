cf143a2bf2860aaab66b6d9805ddd70e2c561405
Jose Manuel Obando <57765554+ObandoGarcia@users.noreply.github.com>
2024-09-15 08:57:28 -0600

    Last changes

--- JDBCCourse/src/DemoJdbc.java
+++ JDBCCourse/src/DemoJdbc.java
@@ -15,10 +15,32 @@ public class DemoJdbc {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String username = "postgres";
         String password = "ecuacion";
+        String query = "select * from students";
 
-        Class.forName("org.postgresql.Driver");
+        //Class.forName("org.postgresql.Driver");
         Connection connection = DriverManager.getConnection(url, username, password);
 
         System.out.println("Connection established!");
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+
+        /*resultSet.next();
+        String name = resultSet.getString("firstname");
+        System.out.println(name);*/
+
+        while(resultSet.next()){
+            System.out.print(resultSet.getInt(1) + " -- ");
+            System.out.print(resultSet.getString(2) + " -- ");
+            System.out.print(resultSet.getString(3) + " -- ");
+            System.out.print(resultSet.getString(4));
+            System.out.println();
+        }
+
+        connection.close();
+
+        System.out.println("Connection close!");
+
+
     }
 }
